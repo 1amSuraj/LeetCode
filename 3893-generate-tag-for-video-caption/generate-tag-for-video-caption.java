d571a57186ff43be7a4c3399323f42d5f2d6ea88
Suraj Srivastava <118651437+1amSuraj@users.noreply.github.com>
2025-06-15 12:48:16 +0530

    Time: 11 ms (100.00%) | Memory: 45.2 MB (100.00%) - LeetSync

--- 3893-generate-tag-for-video-caption/generate-tag-for-video-caption.java
+++ 3893-generate-tag-for-video-caption/generate-tag-for-video-caption.java
@@ -1,18 +1,18 @@
 // class Solution {
 //     public String generateTag(String caption) {
-        
+
 //         String[] s = caption.trim().split("\\s+");
 //         if(s.length<1) return "#";
 //         System.out.println(s.length);
 //         s[0]=s[0].toLowerCase();
-       
+
 //         for(int i =1;i<s.length;i++){
 //             s[i]=s[i].toLowerCase();
 //             StringBuilder sb = new StringBuilder(s[i]);
 //             sb.setCharAt(0,Character.toUpperCase(sb.charAt(0)));
 //             s[i]=sb.toString();
 //         }
-        
+
 //         String res = "#";
 //         for(int i =0;i<s.length;i++){
 //             res+=s[i];
@@ -22,10 +22,10 @@
 //     }
 // }
 
-
 class Solution {
     public String generateTag(String caption) {
-        if (caption == null || caption.trim().isEmpty()) return "#";
+        if (caption == null || caption.trim().isEmpty())
+            return "#";
 
         String[] s = caption.trim().split("\\s+");
         StringBuilder res = new StringBuilder("#");
@@ -35,10 +35,11 @@ class Solution {
 
         // Capitalize first letter of remaining words and make rest lowercase
         for (int i = 1; i < s.length; i++) {
-            if (s[i].isEmpty()) continue;
+            // if (s[i].isEmpty())
+            //     continue;
             String word = s[i].toLowerCase();
             res.append(Character.toUpperCase(word.charAt(0)))
-               .append(word.substring(1));
+                    .append(word.substring(1));
         }
 
         return res.length() > 100 ? res.substring(0, 100) : res.toString();
