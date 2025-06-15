a725467ece64c3a17acf0628fa4ed789f261f6b9
Suraj Srivastava <118651437+1amSuraj@users.noreply.github.com>
2025-06-15 12:47:39 +0530

    Time: 11 ms (100.00%) | Memory: 45.9 MB (100.00%) - LeetSync

--- 3893-generate-tag-for-video-caption/generate-tag-for-video-caption.java
+++ 3893-generate-tag-for-video-caption/generate-tag-for-video-caption.java
@@ -1,23 +1,46 @@
+// class Solution {
+//     public String generateTag(String caption) {
+        
+//         String[] s = caption.trim().split("\\s+");
+//         if(s.length<1) return "#";
+//         System.out.println(s.length);
+//         s[0]=s[0].toLowerCase();
+       
+//         for(int i =1;i<s.length;i++){
+//             s[i]=s[i].toLowerCase();
+//             StringBuilder sb = new StringBuilder(s[i]);
+//             sb.setCharAt(0,Character.toUpperCase(sb.charAt(0)));
+//             s[i]=sb.toString();
+//         }
+        
+//         String res = "#";
+//         for(int i =0;i<s.length;i++){
+//             res+=s[i];
+//         }
+//         if(res.length()>100) return res.substring(0,100);
+//         return res;
+//     }
+// }
+
+
 class Solution {
     public String generateTag(String caption) {
-        
+        if (caption == null || caption.trim().isEmpty()) return "#";
+
         String[] s = caption.trim().split("\\s+");
-        if(s.length<1) return "#";
-        System.out.println(s.length);
-        s[0]=s[0].toLowerCase();
-        // if(s.length==1) return "#"+s[0];
-        for(int i =1;i<s.length;i++){
-            s[i]=s[i].toLowerCase();
-            StringBuilder sb = new StringBuilder(s[i]);
-            sb.setCharAt(0,Character.toUpperCase(sb.charAt(0)));
-            s[i]=sb.toString();
+        StringBuilder res = new StringBuilder("#");
+
+        // Append the first word in lowercase
+        res.append(s[0].toLowerCase());
+
+        // Capitalize first letter of remaining words and make rest lowercase
+        for (int i = 1; i < s.length; i++) {
+            if (s[i].isEmpty()) continue;
+            String word = s[i].toLowerCase();
+            res.append(Character.toUpperCase(word.charAt(0)))
+               .append(word.substring(1));
         }
-        
-        String res = "#";
-        for(int i =0;i<s.length;i++){
-            res+=s[i];
-        }
-        if(res.length()>100) return res.substring(0,100);
-        return res;
+
+        return res.length() > 100 ? res.substring(0, 100) : res.toString();
     }
 }
