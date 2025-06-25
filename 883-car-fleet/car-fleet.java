7054665a7129d090acbe025fe777b63aaa3558a6
Suraj Srivastava <118651437+1amSuraj@users.noreply.github.com>
2025-06-25 13:51:16 +0530

    Time: 48 ms (90.96%) | Memory: 61.8 MB (5.55%) - LeetSync

---
+++ 883-car-fleet/car-fleet.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = position.length;
+        for(int i= 0 ;i<n;i++){
+            map.put(position[i],speed[i]);
+        } 
+        Arrays.sort(position);
+        double[] time = new double[n];
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0;i<n;i++){
+            int dis =target- position[i];
+            time[i] = (double)dis/(double)map.get(position[i]);
+            // stack.push(time[i]);
+        }
+        int c = 1;
+        double maxtillnow = time[n-1];
+        for(int i = n-1;i>0;i--){
+            if(time[i-1]>maxtillnow){
+                c++;
+                maxtillnow = time[i-1];
+            }
+            else{
+
+            }
+        }
+        return c;
+    }
+}
