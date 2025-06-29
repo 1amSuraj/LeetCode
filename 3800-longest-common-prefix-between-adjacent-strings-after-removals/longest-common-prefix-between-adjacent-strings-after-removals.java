9aa7b1a5521c82fa19e01873dd02c9eba9929fbc
Suraj Srivastava <118651437+1amSuraj@users.noreply.github.com>
2025-06-29 10:47:40 +0530

    Time: 27 ms (91.82%) | Memory: 66.7 MB (61.60%) - LeetSync

--- 3800-longest-common-prefix-between-adjacent-strings-after-removals/longest-common-prefix-between-adjacent-strings-after-removals.java
+++ 3800-longest-common-prefix-between-adjacent-strings-after-removals/longest-common-prefix-between-adjacent-strings-after-removals.java
@@ -1,56 +1,44 @@
 class Solution {
     public int[] longestCommonPrefix(String[] words) {
         int n = words.length;
-        int[] ans = new int[n];
-        if (n <= 2) return ans;
-        
-        int[] adjacentPrefix = new int[n - 1];
-        
-        // for each adjacent pair, check prefix length
-        for (int i = 0; i < words.length - 1; i++) {
-            adjacentPrefix[i] = checkPrefixLen(words[i], words[i + 1]);
+        int[] res = new int[n];
+        if (n <= 1) return res;
+
+        int[] lcp = new int[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            lcp[i] = lcpLen(words[i], words[i + 1]);
         }
 
-        int[] prefixMax = new int[n - 1];
-        int[] suffixMax = new int[n - 1];
+        int[] pre = new int[n - 1];
+        int[] suf = new int[n - 1];
 
-        prefixMax[0] = adjacentPrefix[0];
+        pre[0] = lcp[0];
         for (int i = 1; i < n - 1; i++) {
-            prefixMax[i] = Math.max(prefixMax[i - 1], adjacentPrefix[i]);
+            pre[i] = Math.max(pre[i - 1], lcp[i]);
         }
 
-        suffixMax[n - 2] = adjacentPrefix[n - 2];
-        for (int i = n- 3; i >= 0; i--) {
-            suffixMax[i] = Math.max(suffixMax[i + 1], adjacentPrefix[i]);
+        suf[n - 2] = lcp[n - 2];
+        for (int i = n - 3; i >= 0; i--) {
+            suf[i] = Math.max(suf[i + 1], lcp[i]);
         }
 
         for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                ans[i] = suffixMax[1]; 
-            } else if (i == n - 1) {
-                ans[i] = prefixMax[n - 3]; 
-            } else {
-                int left = (i - 2 >= 0) ? prefixMax[i - 2] : 0;
-                int right = (i + 1 < n - 1) ? suffixMax[i + 1] : 0;
-                int merged = checkPrefixLen(words[i - 1], words[i + 1]);
-                ans[i] = Math.max(merged, Math.max(left, right));
+            int best = 0;
+            if (i - 2 >= 0) best = Math.max(best, pre[i - 2]);
+            if (i + 1 <= n - 2) best = Math.max(best, suf[i + 1]);
+            if (i > 0 && i < n - 1) {
+                best = Math.max(best, lcpLen(words[i - 1], words[i + 1]));
             }
+            res[i] = best;
         }
 
-        return ans;
+        return res;
     }
 
-    private int checkPrefixLen(String s1, String s2) {
-        int currPrefixLen = 0;
-
-        int len = Math.min(s1.length(), s2.length());
-        while (currPrefixLen < len) {
-            if (s1.charAt(currPrefixLen) == s2.charAt(currPrefixLen)) {
-                currPrefixLen++;
-            } else {
-                break;
-            }
-        }
-        return currPrefixLen;
+    public static int lcpLen(String a, String b) {
+        int m = Math.min(a.length(), b.length());
+        int i = 0;
+        while (i < m && a.charAt(i) == b.charAt(i)) i++;
+        return i;
     }
 }
