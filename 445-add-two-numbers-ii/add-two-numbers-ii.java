df9fe6f37b0374c2ed8b30d696ef6855a5abfbc0
Suraj Srivastava <118651437+1amSuraj@users.noreply.github.com>
2025-07-01 18:01:03 +0530

    Time: 2 ms (63.37%) | Memory: 44.9 MB (22.83%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/add-two-numbers-ii.java
@@ -10,26 +10,19 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-                // System.out.println(l2.val);
-        // System.out.print(l2.next.val);
+
         ListNode h1 = reverse(l1);
         ListNode h2 = reverse(l2);
         int length1 = checkLength(h1);
         int length2 = checkLength(h2);
         ListNode temp = null;
-        // System.out.println(h2.val);
-        // System.out.print(h2.next.val);
-        // System.out.println(h1.val);
+
         if (length1 < length2) {
             temp = h1;
             h1 = h2;
             h2 = temp;
         }
-        // System.out.println(h1.val);
-        // System.out.print(h1.next.val);
-        // System.out.println(h2.val);
-        // System.out.println(h2.next.val);
-        // System.out.println(length1+" "+length2);
+
         temp = h1;
         int carry = 0;
         ListNode prev= null;
@@ -45,8 +38,6 @@ class Solution {
             int val = h1.val + carry;
             carry= val/ 10;
             h1.val =val% 10;
-        // System.out.println(1);
-            // if (h1.next == null) break;
             prev = h1;
             h1 = h1.next;
         }
@@ -62,9 +53,6 @@ class Solution {
             return head;
         ListNode temp = reverse(head.next);
         head.next.next = head;
-        // System.out.println(head.next.next.val);
-        // System.out.println(temp.val);
-        // System.out.println(temp.next.val);
         head.next = null;
         return temp;
     }
