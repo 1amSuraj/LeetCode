588d5b2fc2804ca05528a162f738e60517ed8a11
Suraj Srivastava <118651437+1amSuraj@users.noreply.github.com>
2025-07-03 11:49:35 +0530

    Time: 17 ms (77.80%) | Memory: 58.6 MB (8.27%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.java
+++ 310-minimum-height-trees/minimum-height-trees.java
@@ -14,12 +14,16 @@ class Solution {
         int farNode[] = new int[1];
         int maxDepth[] = new int[1];
         dfs(0,-1,vis,adj,0,farNode,maxDepth);
-        System.out.println(farNode[0]+" "+maxDepth[0]);
+        // System.out.println(farNode[0]+" "+maxDepth[0]);
+
+
         int parent[] = new int[n+1];
         vis= new int[n+1];
-        Arrays.fill(parent,-1);
+        // Arrays.fill(parent,-1);
         maxDepth[0] = -1;
         dfs2(farNode[0],-1,vis,adj,0,farNode,maxDepth,parent);
+
+
         List<Integer> path = new ArrayList<>();
         int u = farNode[0];
         while(u!=-1){
@@ -27,6 +31,7 @@ class Solution {
             u = parent[u];
         }
         int size = path.size();
+
         if(size%2!=0) {
             return List.of(path.get(size / 2));
         }
