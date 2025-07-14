2d1055339a8c42c03b7152871c2922b3e5ff1386
Suraj Srivastava <118651437+1amSuraj@users.noreply.github.com>
2025-07-14 16:17:31 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (5.27%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -2,21 +2,29 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         if(n==1) return nums[0];
-        int a1[]= Arrays.copyOfRange(nums,1,n);
-        int a2[]= Arrays.copyOfRange(nums,0,n-1);
-        return Math.max(decide(a1),decide(a2));
+        int dp1[] = new int[n+1];
+        int dp2[] = new int[n+1];
+        Arrays.fill(dp1,-1);
+        Arrays.fill(dp2,-1);
+        int p1  = decide1(n-1,nums,dp1);
+        int p2  = decide2(n-2,nums,dp2);
+        return Math.max(p1,p2);
     }
-    public static int decide(int[] nums){
-        int n=nums.length;
-        int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
-        dp[0]=nums[0];
-        for(int i =1 ; i<n; i++){
-            int pick = nums[i];
-            if(i>=2) pick+=dp[i-2]; 
-            int npick = dp[i-1];
-            dp[i]=Math.max(pick,npick);
-        }   
-        return dp[n-1];
+
+    public static int decide2(int i , int[] nums , int[] dp2){
+        if(i<0) return 0;
+        if(i==0) return nums[0];
+        if(dp2[i]!=-1) return dp2[i];
+        int npick = decide2(i-1,nums,dp2);
+        int pick  = nums[i] + decide2(i-2,nums,dp2);
+        return dp2[i] = Math.max(pick, npick);
+    }
+    public static int decide1(int i , int[] nums , int[] dp1){
+        if(i<1) return 0;
+        if(i==1) return nums[1];
+        if(dp1[i]!=-1) return dp1[i];
+        int npick = decide1(i-1,nums,dp1);
+        int pick  = nums[i] + decide1(i-2,nums,dp1);
+        return dp1[i] = Math.max(pick, npick);
     }
 }
