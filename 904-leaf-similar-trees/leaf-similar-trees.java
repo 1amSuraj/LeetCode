5b46c5117606946f6748a567c9fd514b09e48c19
Suraj Srivastava <118651437+1amSuraj@users.noreply.github.com>
2025-07-15 19:26:11 +0530

    Time: 7 ms (4.87%) | Memory: 42 MB (13.92%) - LeetSync

---
+++ 904-leaf-similar-trees/leaf-similar-trees.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        leafWord(root1,sb1); 
+        leafWord(root2,sb2); 
+        System.out.println(sb1.toString());
+        System.out.println(sb2.toString());
+        return sb1.toString().equals(sb2.toString() )? true:false;
+    }
+    public void leafWord(TreeNode root, StringBuilder sb){
+        if(root == null) return;
+        if(root.left== null && root.right == null) {
+            sb.append(root.val+",");
+        }
+        leafWord(root.left,sb);
+        leafWord(root.right,sb);
+    }
+}
