9482991798fd48b46ec0bd6016261fead4fa7a40
Suraj Srivastava <118651437+1amSuraj@users.noreply.github.com>
2025-08-10 03:29:36 +0530

    Time: 2 ms (66.89%) | Memory: 42.9 MB (21.05%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,54 +1,51 @@
 class Pair{
-    int row;
-    int col;
-    int tm;
-    Pair(int row,int col, int tm){
-        this.row=row;
-        this.col=col;
-        this.tm = tm;
+        int row, col , level ;
+        Pair(int row, int col, int level){
+            this.row = row;
+            this.col = col;
+            this.level = level;
+        }
     }
-}
 class Solution {
+    
     public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int vis[][] = new int[n][m];
-        Queue<Pair> queue = new LinkedList<>();
-        int cnt=0;
-        for(int i= 0;i<n;i++){
-            for(int j = 0 ;j<m;j++){
+        int m = grid.length;
+        int n = grid[0].length;
+        int vis[][] = new int[m][n];
+        int cnt=0; // to count number of fresh orange and later compare with visited oranges
+        Queue<Pair> queue= new LinkedList<>();
+
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n; j++){
                 if(grid[i][j]==2){
                     queue.offer(new Pair(i,j,0));
                     vis[i][j] = 1;
                 }
-                else{
-                    vis[i][j] = 0;
-                }
-                if(grid[i][j]==1) cnt++;
+                else if(grid[i][j]==1) cnt++;
             }
         }
-        int count = 0;
-        int tm = 0;
+        int count = 0 ;
+        int lvl = 0;
         int drow[] = {-1,0,1,0};
         int dcol[] = {0,1,0,-1};
         while(!queue.isEmpty()){
-            Pair node = queue.poll();
-            int r = node.row;
-            int c = node.col;
-            int t = node.tm;
-            tm=Math.max(tm,t);
-            for(int i = 0;i<4;i++){
+            Pair pair = queue.poll();
+            int r = pair.row;
+            int c = pair.col;
+            int l = pair.level;
+            lvl = Math.max(lvl,l);
+            for(int i =0;i<4;i++){
                 int nrow = r + drow[i];
                 int ncol = c + dcol[i];
-                if(nrow>= 0 && ncol>=0&&nrow<n && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                    queue.offer(new Pair(nrow,ncol,tm+1));
+                if(nrow>= 0 && ncol>=0&&nrow<m && ncol<n && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
                     vis[nrow][ncol] = 1;
+                    queue.offer(new Pair(nrow,ncol,lvl+1));
                     count++;
                 }
-
             }
+
         }
         if(count!=cnt) return -1;
-        return tm;
+        return lvl;
     }
 }
