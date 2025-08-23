e4a11af84ce65403c83803d80a5459f1e1554112
Suraj Srivastava <118651437+1amSuraj@users.noreply.github.com>
2025-08-23 23:51:14 +0530

    Time: 6 ms (73.21%) | Memory: 44.6 MB (79.33%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,19 +1,38 @@
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         HashMap<Character, Integer> map= new HashMap<>();
+//         int l =0;
+//         int n = s.length();
+//         int max = 0 ;
+//         for(int i = 0 ;i<n;i++){
+//             char ch = s.charAt(i);
+//             if(map.containsKey(ch)){
+//                 l = Math.max(l, map.get(ch) + 1);  // Don't move l backward
+//                 map.remove(ch);
+//             }
+//             map.put(ch,i);
+//             max = Math.max(max, i-l+1);
+//         }
+//         return max;
+//     }
+// }
+
+
+//HashSet method
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashMap<Character, Integer> map= new HashMap<>();
-        int l =0;
-        int r = 0;
-        int n = s.length();
-        int max = 0 ;
-        for(int i = 0 ;i<n;i++){
-            char ch = s.charAt(i);
-            if(map.containsKey(ch)){
-                l = Math.max(l, map.get(ch) + 1);  // Don't move l backward
-                map.remove(ch);
+        HashSet<Character>seen=new HashSet<>();
+        int left=0;
+        int maxlength=0;
+        for(int right=0;right<s.length();right++){
+            while(seen.contains(s.charAt(right))){
+                seen.remove(s.charAt(left));
+                left++;
             }
-            map.put(ch,i);
-            max = Math.max(max, i-l+1);
+            seen.add(s.charAt(right));
+            maxlength=Math.max(maxlength,right-left+1);
         }
-        return max;
+        return maxlength;
     }
 }
