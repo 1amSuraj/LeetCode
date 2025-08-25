78821c59a7156b3dfbdb92930bdf6334aba8d898
Suraj Srivastava <118651437+1amSuraj@users.noreply.github.com>
2025-08-25 21:06:18 +0530

    Time: 54 ms (91.59%) | Memory: 60.9 MB (9.95%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,25 +1,21 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        HashMap<Integer, Integer> map = new HashMap<>();
         int n = position.length;
-        for(int i= 0 ;i<n;i++){
-            map.put(position[i],speed[i]);
-        } 
-        Arrays.sort(position);
-        double[] time = new double[n];
-
-        for(int i = 0;i<n;i++){
-            int dis =target- position[i];
-            time[i] = (double)dis/(double)map.get(position[i]);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(position[i], speed[i]);
         }
-        int c = 1;
-        double maxtillnow = time[n-1];
-        for(int i = n-1;i>0;i--){
-            if(time[i-1]>maxtillnow){
-                c++;
-                maxtillnow = time[i-1];
+        Arrays.sort(position); 
+        Stack<Double> stk = new Stack<>();
+        
+        for (int i = n - 1; i >= 0; i--) {
+            double time = (double)(target - position[i]) / map.get(position[i]); 
+            if (!stk.isEmpty() && time <= stk.peek()) {
+                // joins the fleet ahead → do nothing
+            } else {
+                stk.push(time); // new fleet
             }
         }
-        return c;
+        return stk.size();
     }
 }
