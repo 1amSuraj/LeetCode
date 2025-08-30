2c4d70af6555e05a97c326e1e23d4851b6a7b57b
Suraj Srivastava <118651437+1amSuraj@users.noreply.github.com>
2025-08-31 01:35:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (37.78%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,30 +1,30 @@
+import java.util.*;
+
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n==1) return nums[0];
-        int dp1[] = new int[n+1];
-        int dp2[] = new int[n+1];
-        Arrays.fill(dp1,-1);
-        Arrays.fill(dp2,-1);
-        int p1  = decide1(n-1,nums,dp1);
-        int p2  = decide2(n-2,nums,dp2);
-        return Math.max(p1,p2);
+        if (n == 1) return nums[0];
+
+        // Case 1: Rob [0...n-2]
+        int[] memo1 = new int[n];
+        Arrays.fill(memo1, -1);
+        int option1 = robHelper(nums, 0, n - 2, memo1);
+
+        // Case 2: Rob [1...n-1]
+        int[] memo2 = new int[n];
+        Arrays.fill(memo2, -1);
+        int option2 = robHelper(nums, 1, n - 1, memo2);
+
+        return Math.max(option1, option2);
     }
 
-    public static int decide2(int i , int[] nums , int[] dp2){
-        if(i<0) return 0;
-        if(i==0) return nums[0];
-        if(dp2[i]!=-1) return dp2[i];
-        int npick = decide2(i-1,nums,dp2);
-        int pick  = nums[i] + decide2(i-2,nums,dp2);
-        return dp2[i] = Math.max(pick, npick);
-    }
-    public static int decide1(int i , int[] nums , int[] dp1){
-        if(i<1) return 0;
-        if(i==1) return nums[1];
-        if(dp1[i]!=-1) return dp1[i];
-        int npick = decide1(i-1,nums,dp1);
-        int pick  = nums[i] + decide1(i-2,nums,dp1);
-        return dp1[i] = Math.max(pick, npick);
+    private int robHelper(int[] nums, int i, int end, int[] memo) {
+        if (i > end) return 0;
+        if (memo[i] != -1) return memo[i];
+
+        int pick = nums[i] + robHelper(nums, i + 2, end, memo);
+        int notPick = robHelper(nums, i + 1, end, memo);
+
+        return memo[i] = Math.max(pick, notPick);
     }
 }
