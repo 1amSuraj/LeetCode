2f9691905c55ab1aef30b8fbcb6fab53f562f3a2
Suraj Srivastava <118651437+1amSuraj@users.noreply.github.com>
2026-01-03 17:46:17 +0530

    Time: 3 ms (97.82%) | Memory: 52.5 MB (7.43%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,52 +1,17 @@
-// class Solution {
-//     public int longestOnes(int[] nums, int k) {
-//         int n = nums.length;
-//         int c = 0 ;
-//         int left = 0 ;
-//         int max= 0;
-//         Queue<Integer> queue = new LinkedList<>();
-//         for(int right = 0; right<n;right++){
-//             if(nums[right]==0){
-//                 queue.offer(right);
-//                 if(c==k){
-//                     left = Math.max(left,queue.poll()+1);
-//                 }
-//                 else{
-//                     c++;
-//                 }
-//             }
-//             max = Math.max(max, right-left+1);
-//         }
-//         return max;
-//     }
-// }
-class Solution{
-    public int longestOnes(int[] nums, int k){
-        int c = 0;
-        int max = 0;
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int zerocount = 0;
         int left = 0;
-        Queue<Integer> queue = new LinkedList<>();
-        if(k==0){
-            for(int i = 0;i<nums.length;i++){
-                if(nums[i]==1) c++;
-                if(nums[i]==0) c =0;
-                max= Math.max(max,c);
+        int max = 0;
+        for(int right = 0; right< nums.length; right++){
+            if(nums[right]==0){
+                zerocount++;
             }
-            return max;
-        }
-        for(int right = 0;right<nums.length; right++){
-            if(nums[right]==0  && c==k){
-                while(queue.poll()!=0){
-                    // left++;
-                }
-                c--;
+            while(zerocount > k){
+                if(nums[left]==0) zerocount--;
+                left++;
             }
-            if(nums[right]==1) queue.offer(1);
-            if(nums[right]==0) {
-                queue.offer(0);
-                c++;
-            }
-            max = Math.max(max, queue.size());
+            max =  Math.max(max, right-left+1 );
         }
         return max;
     }
